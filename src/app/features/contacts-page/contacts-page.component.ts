5bbe8cc8dbf460bd1815859be47b27bc81848572
CHXST <christopher.haensch@web.de>
2025-08-04 17:46:50 +0200

    feat: renamed pages folder to 'features'

---
+++ src/app/features/contacts-page/contacts-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/header/header.component';
+
+@Component({
+  selector: 'app-contacts-page',
+  imports: [HeaderComponent],
+  templateUrl: './contacts-page.component.html',
+  styleUrl: './contacts-page.component.scss',
+})
+export class ContactsPageComponent {}
