f0f23c0ed2bf35b953d5c3ea9afeb94be450ac48
CHXST <christopher.haensch@web.de>
2025-08-04 13:33:13 +0200

    feat: added angular-fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'join-mmc',
+        appId: '1:743995687615:web:112ab39b5754a8ca7bfc0a',
+        storageBucket: 'join-mmc.firebasestorage.app',
+        apiKey: 'AIzaSyAAX2A5vEX7JJbae6szFNRiLN6-U7aRaXI',
+        authDomain: 'join-mmc.firebaseapp.com',
+        messagingSenderId: '743995687615',
+        measurementId: 'G-Y0RYN7K0JQ',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
