6d8b82af817a32b2036d503ebde5a74f8e1453d6
Miles <miles.pickull@web.de>
2025-08-04 16:51:39 +0200

    added user menu and made menu open and closeable

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
+  activeFlex = false;
+
+  toggleMenu() {
+    if (this.activeFlex) {
+      this.activeFlex = false;
+    } else {
+      this.activeFlex = true;
+    }
+  }
 }
