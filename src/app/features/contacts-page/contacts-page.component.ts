dad6a8d9c259718fe00cd106fc6f26f3ef071c0b
Marc <marc.doehring@icloud.com>
2025-08-05 12:10:03 +0200

    feat: refractor button component wip

--- src/app/features/contacts-page/contacts-page.component.ts
+++ src/app/features/contacts-page/contacts-page.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+import { ButtonComponent } from '../../shared/ui/button/button.component';
 
 @Component({
   selector: 'app-contacts-page',
-  imports: [HeaderComponent, ContactsListComponent, ContactDetailsComponent],
+  imports: [HeaderComponent, ContactsListComponent, ContactDetailsComponent, ButtonComponent],
   templateUrl: './contacts-page.component.html',
   styleUrl: './contacts-page.component.scss',
 })
