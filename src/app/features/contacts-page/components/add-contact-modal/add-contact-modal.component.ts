fadfa5924fd352ef99383770d55332ea7277bc68
Marc <marc.doehring@icloud.com>
2025-08-05 21:48:09 +0200

    feat: added add-contact-modal svg´s for contactform

--- src/app/features/contacts-page/components/add-contact-modal/add-contact-modal.component.ts
+++ src/app/features/contacts-page/components/add-contact-modal/add-contact-modal.component.ts
@@ -1,15 +1,18 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { ButtonComponent } from '../../../../shared/ui/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-contact-modal',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CommonModule],
   templateUrl: './add-contact-modal.component.html',
   styleUrls: ['./add-contact-modal.component.scss'],
 })
 export class AddContactModalComponent {
   @Output() close = new EventEmitter<void>();
+
+  isOpen = true;
 
   closeModal() {
     this.close.emit();
