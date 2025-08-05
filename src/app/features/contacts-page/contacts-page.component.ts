301d5484c73e69454fb643f22c1a0a2d818176e6
CHXST <christopher.haensch@web.de>
2025-08-05 14:16:20 +0200

    feat: added temporary methods to create contacts

--- src/app/features/contacts-page/contacts-page.component.ts
+++ src/app/features/contacts-page/contacts-page.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
 import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
+import { ContactsService } from '../../services/firebase/contacts.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacts-page',
-  imports: [HeaderComponent, ContactsListComponent, ContactDetailsComponent],
+  imports: [
+    HeaderComponent,
+    ContactsListComponent,
+    ContactDetailsComponent,
+    FormsModule,
+  ],
   templateUrl: './contacts-page.component.html',
   styleUrl: './contacts-page.component.scss',
 })
-export class ContactsPageComponent {}
+export class ContactsPageComponent {
+  contact = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+  };
+
+  contactsService: ContactsService = inject(ContactsService);
+
+  submitContact() {
+    let tempContact = this.generatePersonData(
+      this.contact.firstName,
+      this.contact.lastName
+    );
+    this.contactsService.addContactToDatabase({ id: '', ...tempContact });
+    this.contact.firstName = '';
+    this.contact.lastName = '';
+  }
+
+  // temp function
+  generatePersonData(fN: string, lN: string) {
+    const email = `${fN.toLowerCase()}.${lN.toLowerCase()}@mail.com`;
+
+    // random but real looking phone number
+    const mobilePrefixes = [
+      '0151',
+      '0160',
+      '0170',
+      '0171',
+      '0175',
+      '0176',
+      '0177',
+      '0178',
+      '0179',
+      '0162',
+      '0163',
+      '0157',
+      '0159',
+    ];
+    const randomPrefix =
+      mobilePrefixes[Math.floor(Math.random() * mobilePrefixes.length)];
+    const randomNumber = Math.floor(10000000 + Math.random() * 90000000); // random 8 digit number
+    const phoneNumber = `${randomPrefix}${randomNumber}`;
+
+    return {
+      firstName: fN,
+      lastName: lN,
+      email: email,
+      phoneNumber: phoneNumber,
+    };
+  }
+}
