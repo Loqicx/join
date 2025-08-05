6329b197fc45f6fc9417c42d4ab8a267540bb312
CHXST <christopher.haensch@web.de>
2025-08-05 09:45:50 +0200

    feat: add 'contact-list' and 'contact-details' component to contact page

--- src/app/features/contacts-page/contacts-page.component.ts
+++ src/app/features/contacts-page/contacts-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { ContactsListComponent } from './components/contacts-list/contacts-list.component';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
 
 @Component({
   selector: 'app-contacts-page',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ContactsListComponent, ContactDetailsComponent],
   templateUrl: './contacts-page.component.html',
   styleUrl: './contacts-page.component.scss',
 })
