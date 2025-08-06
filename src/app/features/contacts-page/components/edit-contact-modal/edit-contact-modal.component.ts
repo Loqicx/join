07a6f4d963df7c3e08bfb5d7dc126c152c5af400
Marc <marc.doehring@icloud.com>
2025-08-06 14:57:45 +0200

    feat: added edit-contact-modal saveContact deleteContact WIP

--- src/app/features/contacts-page/components/edit-contact-modal/edit-contact-modal.component.ts
+++ src/app/features/contacts-page/components/edit-contact-modal/edit-contact-modal.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, inject } from '@angular/core';
+import { Contact } from '../../../../shared/interfaces/contact';
+import { ContactsService } from '../../../../services/firebase/contacts.service';
+import { ButtonComponent } from '../../../../shared/ui/button/button.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-contact-modal',
-  imports: [],
+  standalone: true,
+  imports: [ButtonComponent, CommonModule, FormsModule],
   templateUrl: './edit-contact-modal.component.html',
-  styleUrl: './edit-contact-modal.component.scss'
+  styleUrls: ['./edit-contact-modal.component.scss'],
 })
 export class EditContactModalComponent {
+  @Input() contactToEdit!: Contact;
+  @Output() close = new EventEmitter<void>();
 
+  isOpen = true;
+  contactsService = inject(ContactsService);
+  fullName = '';
+
+  ngOnInit() {
+    if (this.contactToEdit) {
+      this.fullName = `${this.contactToEdit.firstName} ${this.contactToEdit.lastName}`;
+    }
+  }
+
+  async saveContact() {
+    const nameParts = this.fullName.trim().split(' ');
+    this.contactToEdit.firstName = nameParts[0];
+    this.contactToEdit.lastName = nameParts.slice(1).join(' ');
+
+    try {
+      await this.contactsService.updateContactInDatabase(this.contactToEdit);
+      this.close.emit();
+    } catch (error) {
+      console.error('Update failed:', error);
+    }
+  }
+
+  async deleteContact() {
+    try {
+      await this.contactsService.deleteContactFromDatabase(this.contactToEdit.id);
+      this.close.emit();
+    } catch (error) {
+      console.error('Delete failed:', error);
+    }
+  }
+
+  closeModal() {
+    this.close.emit();
+  }
 }
