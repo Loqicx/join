a0bbc4b5ce463c0fc39b5bef6f41aa5b6affd0b9
miles <miles.pickull@web.de>
2025-08-08 00:10:22 +0200

    feat: (help-page component) added arrow "back"

--- src/app/features/help-page/help-page.component.ts
+++ src/app/features/help-page/help-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-help-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './help-page.component.html',
   styleUrl: './help-page.component.scss'
 })
