ed84bc2a0d4f0d4dde4a396b2193064a658e4576
Marc <marc.doehring@icloud.com>
2025-08-08 11:30:18 +0200

    feat: (contact-component, edit-contact-modal) add contact details and edit modal

--- src/app/features/contacts-page/components/edit-contact-modal/edit-contact-modal.component.ts
+++ src/app/features/contacts-page/components/edit-contact-modal/edit-contact-modal.component.ts
@@ -16,9 +16,11 @@ export class EditContactModalComponent {
   @Input() contactToEdit!: Contact;
   @Output() close = new EventEmitter<void>();
 
-  isOpen = true;
+  isOpen = false;
   contactsService = inject(ContactsService);
   fullName = '';
+
+  contact: Contact | null = null;
 
   ngOnInit() {
     if (this.contactToEdit) {
@@ -26,29 +28,44 @@ export class EditContactModalComponent {
     }
   }
 
-  // async saveContact() {
-  //   const nameParts = this.fullName.trim().split(' ');
-  //   this.contactToEdit.firstName = nameParts[0];
-  //   this.contactToEdit.lastName = nameParts.slice(1).join(' ');
+  openModal(contactData: Contact) {
+    this.contact = { ...contactData };
+    this.fullName = `${contactData.firstName} ${contactData.lastName}`;
+    this.isOpen = true;
+  }
 
-  //   try {
-  //     await this.contactsService.updateContactInDatabase(this.contactToEdit);
-  //     this.close.emit();
-  //   } catch (error) {
-  //     console.error('Update failed:', error);
-  //   }
-  // }
+  async saveContact() {
+    if (!this.contact) {
+      console.error('No contact loaded');
+      return;
+    }
 
-  // async deleteContact() {
-  //   try {
-  //     await this.contactsService.deleteContactFromDatabase(this.contactToEdit.id);
-  //     this.close.emit();
-  //   } catch (error) {
-  //     console.error('Delete failed:', error);
-  //   }
-  // }
+    const nameParts = this.fullName.trim().split(' ');
+    this.contact.firstName = nameParts[0];
+    this.contact.lastName = nameParts.slice(1).join(' ');
+
+    try {
+      await this.contactsService.updateContact(this.contact, this.contact.id);
+      this.close.emit();
+    } catch (error) {
+      console.error('Update failed:', error);
+    }
+  }
+
+  async deleteContact() {
+  if (!this.contact?.id) {
+    console.error('No contact to delete or missing id');
+    return;
+  }
+  try {
+    await this.contactsService.deleteContact(this.contact.id);
+    this.close.emit();
+  } catch (error) {
+    console.error('Delete failed:', error);
+  }
+}
 
   closeModal() {
-    this.close.emit();
+    this.isOpen = false;
   }
 }
