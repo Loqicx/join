238d015a66964f7f1e512c1327affa4660f7a072
CHXST <christopher.haensch@web.de>
2025-08-11 14:52:56 +0200

    feat(InitialLetterService): make sure initials are uppercase

--- src/app/shared/services/get-initial-letters.service.ts
+++ src/app/shared/services/get-initial-letters.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class InitialLettersService {
   constructor() {}
   getInitialLetters(contact: any): String {
-    let firstNameInitial = contact.firstName.charAt(0);
-    let lastNameInitial = contact.lastName.charAt(0);
+    let firstNameInitial = contact.firstName.charAt(0).toUpperCase();
+    let lastNameInitial = contact.lastName.charAt(0).toUpperCase();
 
     return firstNameInitial + lastNameInitial;
   }
