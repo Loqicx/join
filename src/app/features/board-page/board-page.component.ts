ae83efbe5c9d6ef2827b177bf3539f7d1969f05e
CHXST <christopher.haensch@web.de>
2025-08-18 15:16:00 +0200

    feat(board-page): implement task-service in board-page

--- src/app/features/board-page/board-page.component.ts
+++ src/app/features/board-page/board-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ButtonComponent } from '../../shared/ui/button/button.component';
 import {
   CdkDragDrop,
@@ -10,6 +10,7 @@ import {
 } from '@angular/cdk/drag-drop';
 import { TaskCardComponent } from '../../shared/task-card/task-card.component';
 import { TasksService } from '../../shared/services/firebase/tasks.service';
+import { Task } from '../../shared/interfaces/task';
 
 @Component({
   selector: 'app-board-page',
@@ -23,36 +24,46 @@ import { TasksService } from '../../shared/services/firebase/tasks.service';
   templateUrl: './board-page.component.html',
   styleUrl: './board-page.component.scss',
 })
-export class BoardPageComponent {
-  tasks;
-  board = {
+export class BoardPageComponent implements OnInit {
+  tasks: Task[] = [];
+  board: { lists: { name: string; listIndex: number; items: Task[] }[] } = {
     lists: [
       {
         name: 'To do',
-        items: ['Header einbauen', 'Footer einbauen'],
+        listIndex: 1,
+        items: [],
       },
       {
         name: 'In progress',
-        items: ['Kochen', 'Putzen'],
+        listIndex: 2,
+        items: [],
       },
       {
         name: 'Await feedback',
-        items: ['Kontaktliste bauen'],
+        listIndex: 3,
+        items: [],
       },
       {
         name: 'Done',
-        items: ['Hausaufgaben'],
+        listIndex: 4,
+        items: [],
       },
     ],
   };
 
   tasksService: TasksService = inject(TasksService);
 
-  constructor() {
-    this.tasks = this.tasksService.tasks;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.tasksService.tasks$.subscribe((tasks) => {
+      this.tasks = tasks;
+      console.log(this.tasks);
+      this.handleTaskUpdate();
+    });
   }
 
-  drop(event: CdkDragDrop<string[]>) {
+  drop(event: CdkDragDrop<Task[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(
         event.container.data,
@@ -66,7 +77,29 @@ export class BoardPageComponent {
         event.previousIndex,
         event.currentIndex
       );
+
+      const targetListId = event.container.id;
+      const targetListIndex = parseInt(targetListId.replace('list-', ''));
+      const updateData = {
+        status: targetListIndex + 1,
+      };
+      this.tasksService.updateTask(
+        updateData,
+        event.container.data[event.currentIndex].id
+      );
     }
+  }
+  handleTaskUpdate() {
+    this.clearList();
+    this.tasks.forEach((task) => {
+      this.board.lists[task.status! - 1].items.push(task);
+    });
+  }
+
+  clearList() {
+    this.board.lists.forEach((list) => {
+      list.items = [];
+    });
   }
 
   others(currentListName: string) {
