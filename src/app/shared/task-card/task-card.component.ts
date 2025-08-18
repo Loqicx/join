7c957dc79e417434572469c8108814de109ed912
Marc <marc.doehring@icloud.com>
2025-08-18 22:36:00 +0200

    feat: task-card-component assignedInitials

--- src/app/shared/task-card/task-card.component.ts
+++ src/app/shared/task-card/task-card.component.ts
@@ -1,39 +1,54 @@
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../interfaces/task';
 import { TaskCategory } from '../services/firebase/tasks.service';
 import { Contact } from '../interfaces/contact';
 import { ContactsService } from '../services/firebase/contacts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.scss',
+  styleUrls: ['./task-card.component.scss'],
 })
-export class TaskCardComponent implements OnInit {
+export class TaskCardComponent implements OnInit, OnDestroy {
   @Input() task!: Task;
   taskTechnical: boolean = false;
   taskUserStory: boolean = false;
   taskCategory: string = '';
 
-  assignedContacts: Contact[] = [];
+  assignedInitials: string[] = [];
 
   contactsService: ContactsService = inject(ContactsService);
 
+  private contactSub: Subscription | null = null;
+
   constructor() {}
 
-  async ngOnInit(): Promise<void> {
+  ngOnInit(): void {
     this.taskCategory = this.getTaskCategory();
+    this.updateAssignedInitials();
+  }
+
+  ngOnDestroy(): void {
+    if (this.contactSub) this.contactSub.unsubscribe();
+  }
+
+  updateAssignedInitials(): void {
+    this.assignedInitials = this.task.assignedTo.map((id: string) => {
+      const contact: Contact | undefined = this.contactsService.getContactById(id);
+      if (contact) {
+        return (contact.firstName.charAt(0) + contact.lastName.charAt(0)).toUpperCase();
+      } else {
+        return '??';
+      }
+    });
   }
 
   subtasksDone(): number {
-    let count = 0;
-    this.task.subtasks.forEach((subtask) => {
-      if (subtask.done) count++;
-    });
-    return count;
+    return this.task.subtasks.filter((s) => s.done).length;
   }
 
   subtasksTotal(): number {
@@ -46,16 +61,12 @@ export class TaskCardComponent implements OnInit {
         this.taskTechnical = true;
         this.taskUserStory = false;
         return 'Technical Task';
-        break;
       case TaskCategory.USER_STORY:
         this.taskTechnical = false;
         this.taskUserStory = true;
         return 'User Story';
-        break;
-
       default:
         return 'Default Task';
-        break;
     }
   }
 }
