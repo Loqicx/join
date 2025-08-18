a18f9fc215c6245c64dac9f81875bda88ebcc21f
CHXST <christopher.haensch@web.de>
2025-08-18 19:19:41 +0200

    revert previous commit 24e2f239d1b95e2e0de1b4d828636e2c57e20900

--- src/app/shared/task-card/task-card.component.ts
+++ src/app/shared/task-card/task-card.component.ts
@@ -12,7 +12,6 @@ import { TaskCategory } from '../services/firebase/tasks.service';
 })
 export class TaskCardComponent {
   @Input() task!: Task;
-  contactsService: ContactsService = inject(ContactsService);
 
   constructor() {}
 
