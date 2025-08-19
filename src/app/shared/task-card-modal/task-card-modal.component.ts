d3d4af785a1502fc21ad98a56f4a2b7d3aa104dd
Marc <marc.doehring@icloud.com>
2025-08-19 12:18:34 +0200

    feat: add task-card-modal component

---
+++ src/app/shared/task-card-modal/task-card-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-card-modal',
+  imports: [],
+  templateUrl: './task-card-modal.component.html',
+  styleUrl: './task-card-modal.component.scss'
+})
+export class TaskCardModalComponent {
+
+}
