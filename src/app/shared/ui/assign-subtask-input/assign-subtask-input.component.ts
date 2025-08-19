dc06fce8b5f4364f57b57b248cc2a9202b6dd459
miles <miles.pickull@web.de>
2025-08-19 02:14:22 +0200

    feat(subtask-component): moved subtask input into new standalone component

---
+++ src/app/shared/ui/assign-subtask-input/assign-subtask-input.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-assign-subtask-input',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './assign-subtask-input.component.html',
+  styleUrl: './assign-subtask-input.component.scss'
+})
+export class AssignSubtaskInputComponent {
+
+  subtasks: { id: number, title: string }[] = [];
+  taskSubtask: any;
+
+  subtaskActive: boolean = false;
+  subtaskInput: boolean = false;
+
+  addSubtask() {
+    if (this.taskSubtask) {
+      this.subtasks.push({ id: this.subtasks.length + 1, title: this.taskSubtask });
+      this.taskSubtask = '';
+      this.subtaskInput = false;
+    }
+    this.subtaskActive = false;
+  }
+}
