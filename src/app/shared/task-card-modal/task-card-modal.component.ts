8c1d8ba1ed17e286153bca8b9ddeb6178934e0dc
CHXST <christopher.haensch@web.de>
2025-08-21 12:23:33 +0200

    feat(task-card-modal): fix category and implement edit and delete button

--- src/app/shared/task-card-modal/task-card-modal.component.ts
+++ src/app/shared/task-card-modal/task-card-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Output, EventEmitter, OnInit, inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnInit,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../interfaces/task';
 import { Contact } from '../interfaces/contact';
@@ -6,6 +13,8 @@ import { ContactsService } from '../services/firebase/contacts.service';
 import { InitialLettersService } from '../services/get-initial-letters.service';
 import { ColoredProfilePipe } from '../pipes/colored-profile.pipe';
 import { TaskCategory } from '../services/firebase/tasks.service';
+import { SVGInlineService } from '../services/svg-inline.service';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-task-card-modal',
@@ -13,9 +22,20 @@ import { TaskCategory } from '../services/firebase/tasks.service';
   imports: [CommonModule],
   templateUrl: './task-card-modal.component.html',
   styleUrls: ['./task-card-modal.component.scss'],
-  providers: [ColoredProfilePipe]
+  providers: [ColoredProfilePipe, SVGInlineService],
 })
 export class TaskCardModalComponent implements OnInit {
+  svgContents: { [key: string]: SafeHtml } = {};
+  icons = [
+    { name: 'edit', src: './assets/icons/edit.svg' },
+    { name: 'delete', src: './assets/icons/delete.svg' },
+  ];
+
+  constructor(
+    private svgService: SVGInlineService,
+    private sanitizer: DomSanitizer
+  ) {}
+
   @Input() task!: Task;
 
   @Output() close = new EventEmitter<void>();
@@ -33,7 +53,11 @@ export class TaskCardModalComponent implements OnInit {
 
   ngOnInit(): void {
     this.updateAssignedContacts();
-    this.getTaskCategory();
+    this.taskCategory = this.getTaskCategory();
+
+    this.icons.forEach((icon) => {
+      this.convertIcon(icon.name, icon.src);
+    });
   }
 
   updateAssignedContacts(): void {
@@ -43,38 +67,54 @@ export class TaskCardModalComponent implements OnInit {
 
     for (let i = 0; i < this.task.assignedTo.length; i++) {
       const id = this.task.assignedTo[i];
-      const contact: Contact | undefined = this.contactsService.getContactById(id);
+      const contact: Contact | undefined =
+        this.contactsService.getContactById(id);
 
       if (contact) {
-        const initials: String = this.initialLetterService.getInitialLetters(contact);
+        const initials: String =
+          this.initialLetterService.getInitialLetters(contact);
         const color: String = this.coloredProfilePipe.transform(contact.id);
         this.assignedContacts.push({
           initials,
           color,
-          name: `${contact.firstName} ${contact.lastName}`
+          name: `${contact.firstName} ${contact.lastName}`,
         });
       } else {
-        this.assignedContacts.push({ initials: '??', color: '#999', name: 'Unknown' });
+        this.assignedContacts.push({
+          initials: '??',
+          color: '#999',
+          name: 'Unknown',
+        });
       }
     }
   }
 
   getTaskCategory(): string {
-      switch (this.task.category) {
-        case TaskCategory.TECHNICAL_TASK:
-          this.taskTechnical = true;
-          this.taskUserStory = false;
-          return 'Technical Task';
-        case TaskCategory.USER_STORY:
-          this.taskTechnical = false;
-          this.taskUserStory = true;
-          return 'User Story';
-        default:
-          this.taskTechnical = false;
-          this.taskUserStory = false;
-          return 'Default Task';
-      }
+    switch (this.task.category) {
+      case TaskCategory.TECHNICAL_TASK:
+        this.taskTechnical = true;
+        this.taskUserStory = false;
+        return 'Technical Task';
+      case TaskCategory.USER_STORY:
+        this.taskTechnical = false;
+        this.taskUserStory = true;
+        return 'User Story';
+      default:
+        this.taskTechnical = false;
+        this.taskUserStory = false;
+        return 'Default Task';
     }
+  }
+
+  convertIcon(iconName: string, iconSrc: string): void {
+    this.svgService.getInlineSVG(iconSrc).subscribe({
+      next: (svg: string) => {
+        this.svgContents[iconName] =
+          this.sanitizer.bypassSecurityTrustHtml(svg);
+      },
+      error: (err) => console.error('SVG load error:', err),
+    });
+  }
 
   onClose() {
     this.close.emit();
