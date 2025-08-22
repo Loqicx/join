841f5852e0b01966a760131c23f7cc26ac73f7d5
miles <miles.pickull@web.de>
2025-08-22 23:01:14 +0200

    feat(date-picker): generated Component

---
+++ src/app/shared/ui/date-picker-input/date-picker-input.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-date-picker-input',
+  imports: [],
+  templateUrl: './date-picker-input.component.html',
+  styleUrl: './date-picker-input.component.scss'
+})
+export class DatePickerInputComponent {
+
+}
