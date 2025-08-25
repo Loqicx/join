9375c54094439b8d4f7c6a6d754c892b75e1734e
Miles <miles.pickull@web.de>
2025-08-25 13:17:57 +0200

    feat(edit-task): added edit task modal component

---
+++ src/app/shared/edit-task-modal/edit-task-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-task-modal',
+  imports: [],
+  templateUrl: './edit-task-modal.component.html',
+  styleUrl: './edit-task-modal.component.scss'
+})
+export class EditTaskModalComponent {
+
+}
