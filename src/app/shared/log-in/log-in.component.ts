1e423fbd0744f79663e09d5e24c5f157aeda42a8
Miles <miles.pickull@web.de>
2025-09-01 13:45:50 +0200

    feat(log-in): added base HTML

--- src/app/shared/log-in/log-in.component.ts
+++ src/app/shared/log-in/log-in.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from "../ui/button/button.component";
 
 @Component({
   selector: 'app-log-in',
-  imports: [],
+  imports: [FormsModule, ButtonComponent],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
 export class LogInComponent {
+logInEmail: any;
+logInPassword: any;
 
+  logIn() {
+    console.log('Here should be a login Logic mady by Loqicx')
+  }
 }
