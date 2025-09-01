1902fdec621c83d7671802179f424a49a7b27028
Miles <miles.pickull@web.de>
2025-09-01 13:47:09 +0200

    feat(log-in): added log-in to page

--- src/app/features/log-in-page/log-in-page.component.ts
+++ src/app/features/log-in-page/log-in-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LogInComponent } from "../../shared/log-in/log-in.component";
 
 @Component({
   selector: 'app-log-in-page',
-  imports: [],
+  imports: [LogInComponent],
   templateUrl: './log-in-page.component.html',
   styleUrl: './log-in-page.component.scss'
 })
