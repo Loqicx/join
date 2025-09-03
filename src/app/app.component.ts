1a9323be3b68440273a762867d8d8c4656c18c07
Miles <miles.pickull@web.de>
2025-09-03 12:05:33 +0200

    feat(app): removed unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactsService } from './shared/services/firebase/contacts.service';
-import { Contact } from './shared/interfaces/contact';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { LogInPageComponent } from "./features/log-in-page/log-in-page.component";
+
 
 @Component({
   selector: 'app-root',
@@ -19,12 +19,22 @@ import { LogInPageComponent } from "./features/log-in-page/log-in-page.component
     SidebarComponent,
     FooterComponent,
     LogInPageComponent
-],
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
+/**
+ * The main application component for the Join MMC project.
+ *
+ * @remarks
+ * - Manages application state such as login status and page view.
+ * - Injects {@link ContactsService} to actualize (refresh) all contacts on page load.
+ */
 export class AppComponent {
   title = 'join-mmc';
+
   loggedIn = false; 
   loginPage = true;
+
+  contactsService: ContactsService = inject(ContactsService);
 }
