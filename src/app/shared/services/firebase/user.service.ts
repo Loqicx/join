51ae9d3f78644b5862df7200ef2f5ae476f12bf5
CHXST <christopher.haensch@web.de>
2025-09-03 12:04:40 +0200

    feat(authentication): added user observable

--- src/app/shared/services/firebase/user.service.ts
+++ src/app/shared/services/firebase/user.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Auth, user, User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class UserService {
-    constructor() {}
+    user$: Observable<User | null>;
+
+    auth = inject(Auth);
+
+    constructor() {
+        this.user$ = user(this.auth);
+    }
 }
