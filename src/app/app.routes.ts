f1c38c649da8bad5bb5e5d63b59596780f65a46e
CHXST <christopher.haensch@web.de>
2025-09-03 12:12:25 +0200

    feat(authentication): example authGuard implementation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,38 +6,43 @@ import { HelpPageComponent } from './features/help-page/help-page.component';
 import { BoardPageComponent } from './features/board-page/board-page.component';
 import { AddTaskPageComponent } from './features/add-task-page/add-task-page.component';
 import { SummaryComponent } from './features/summary/summary.component';
+import { authGuard } from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: ContactsPageComponent, // TODO: add "home" component
-  },
-  {
-    path: 'summary',
-    component: SummaryComponent,
-  },
-  {
-    path: 'board',
-    component: BoardPageComponent,
-  },
-  {
-    path: 'contacts',
-    component: ContactsPageComponent,
-  },
-  {
-    path: 'addTask',
-    component: AddTaskPageComponent,
-  },
-  {
-    path: 'privacy',
-    component: PrivacyPolicyPageComponent,
-  },
-  {
-    path: 'legal',
-    component: LegalNoticePageComponent,
-  },
-  {
-    path: 'help',
-    component: HelpPageComponent,
-  },
+    {
+        path: '',
+        component: ContactsPageComponent, // TODO: add "home" component
+    },
+    {
+        path: 'summary',
+        component: SummaryComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path: 'board',
+        component: BoardPageComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path: 'contacts',
+        component: ContactsPageComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path: 'addTask',
+        component: AddTaskPageComponent,
+        canActivate: [authGuard],
+    },
+    {
+        path: 'privacy',
+        component: PrivacyPolicyPageComponent,
+    },
+    {
+        path: 'legal',
+        component: LegalNoticePageComponent,
+    },
+    {
+        path: 'help',
+        component: HelpPageComponent,
+    },
 ];
