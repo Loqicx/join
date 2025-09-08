d6ee9b7c2cc64a2ccec2660ca6237a0e57ea9064
miles <miles.pickull@web.de>
2025-09-08 21:15:04 +0200

    feat(log-in): add privacy policy and legal notice links to the login page

--- src/app/features/log-in-page/log-in-page.component.ts
+++ src/app/features/log-in-page/log-in-page.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,inject } from '@angular/core';
 import { LogInComponent } from "../../shared/log-in/log-in.component";
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from "../../shared/ui/button/button.component";
+import { RouterLink } from '@angular/router';
+import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-log-in-page',
-  imports: [LogInComponent, CommonModule, ButtonComponent],
+  imports: [LogInComponent, CommonModule, ButtonComponent, RouterLink],
   templateUrl: './log-in-page.component.html',
   styleUrl: './log-in-page.component.scss'
 })
@@ -13,4 +15,11 @@ export class LogInPageComponent {
   @Input() animate: boolean = false;
   @Input() fade: boolean = false;
   signUpShow: boolean = false;
+  appComponent = inject(AppComponent);
+
+  closeLogIn() {
+    this.appComponent.showRouter = true;
+    this.appComponent.loginPage = false;
+    this.appComponent.show = true;
+  }
 }
