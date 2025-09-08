f10577324a68a1c83e569c97e85d86e6efd8bb6f
miles <miles.pickull@web.de>
2025-09-08 21:15:12 +0200

    feat(auth): enhance auth guard to manage app state on user login/logout

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -2,20 +2,24 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { UserService } from '../services/firebase/user.service';
 import { map } from 'rxjs';
+import { AppComponent } from '../../app.component';
 
 export const authGuard: CanActivateFn = (route, state) => {
     const userService = inject(UserService);
     const router = inject(Router);
+    const appComponent = inject(AppComponent);
 
     return userService.user$.pipe(
         map((user) => {
             if (user) {
+                appComponent.showRouter = true;
+                appComponent.loginPage = false;
+                appComponent.show = true;
                 return true;
             } else {
                 // trigger 'not logged in' notification
 
-                // navigate to login page
-                router.navigate(['']);
+                appComponent.resetState();
                 return false;
             }
         })
