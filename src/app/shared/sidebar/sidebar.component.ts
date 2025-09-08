6a37d9154f3923b8fc4de53858cc90effc33a639
miles <miles.pickull@web.de>
2025-09-09 01:44:58 +0200

    feat(sidebar): added functionality to remove user menu when not logged in and updated to routerActive functionality

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouterModule } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { SVGInlineService } from '../services/svg-inline.service';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { LoginService } from '../services/app-login-service.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -15,66 +15,30 @@ import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 export class SidebarComponent {
   svgContents: { [key: string]: SafeHtml } = {};
 
-  contactsActive = false;
-  summaryActive = false;
-  addTaskActive = false;
-  boardActive = false;
-  privacyActive = false;
-  legalActive = false;
+  loggedIn = false;
 
   icons = [
     { name: 'contacts', src: './assets/icons/contacts.svg' },
     { name: 'summary', src: './assets/icons/summary.svg' },
     { name: 'addTask', src: './assets/icons/add-task.svg' },
     { name: 'board', src: './assets/icons/board.svg' },
+    { name: 'login', src: './assets/icons/login.svg' },
   ];
 
-  /**
-   * Constructor for the Sidebar component that initializes routing events and sets active Button based on current URL.
-   *
-   * @param {Router} router - The Angular Router instance used to listen for navigation events.
-   */
+  loginService = inject(LoginService);
+
   constructor(
     private router: Router,
     private svgService: SVGInlineService,
     private sanitizer: DomSanitizer
-  ) {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((event: NavigationEnd) => {
-        const currentUrl = event.urlAfterRedirects;
-        this.resetAllActives();
-        if (currentUrl.startsWith('/contacts')) {
-          this.contactsActive = true;
-        } else if (currentUrl.startsWith('/summary')) {
-          this.summaryActive = true;
-        } else if (currentUrl.startsWith('/addTask')) {
-          this.addTaskActive = true;
-        } else if (currentUrl.startsWith('/board')) {
-          this.boardActive = true;
-        } else if (currentUrl.startsWith('/legal')) {
-          this.legalActive = true;
-        } else if (currentUrl.startsWith('/privacy')) {
-          this.privacyActive = true;
-        }
-      });
-  }
-
-  /**
-   * Resets all active tabs to false.
-   */
-  private resetAllActives() {
-    this.contactsActive = false;
-    this.summaryActive = false;
-    this.addTaskActive = false;
-    this.boardActive = false;
-    this.privacyActive = false;
-    this.legalActive = false;
-  }
+  ) { }
 
   ngOnInit(): void {
     this.icons.forEach((icon) => {
       this.convertIcon(icon.name, icon.src);
+    });
+    this.loginService.actualLogin$.subscribe(isLoggedIn => {
+      this.loggedIn = isLoggedIn;
     });
   }
 
