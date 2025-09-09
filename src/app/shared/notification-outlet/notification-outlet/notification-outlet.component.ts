c68d20d5a804b33818bc1993fd0bea9e6bd7dfd3
CHXST <christopher.haensch@web.de>
2025-09-09 13:39:59 +0200

    feat(notification): add notification outlet component

---
+++ src/app/shared/notification-outlet/notification-outlet/notification-outlet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'notification-outlet',
+    imports: [],
+    templateUrl: './notification-outlet.component.html',
+    styleUrl: './notification-outlet.component.scss',
+})
+export class NotificationOutletComponent {
+    constructor() {}
+}
