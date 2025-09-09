9047621ec00c41c9dad55babd8d6266366354ac4
CHXST <christopher.haensch@web.de>
2025-09-09 13:51:31 +0200

    feat(notification): add logic for notification positioning

--- src/app/shared/notification-outlet/notification-outlet/notification-outlet.component.ts
+++ src/app/shared/notification-outlet/notification-outlet/notification-outlet.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
-import { Notification } from '../../interfaces/notification';
+import { Notification, NotificationPosition } from '../../interfaces/notification';
 
 @Component({
     selector: 'notification-outlet',
@@ -25,5 +25,24 @@ export class NotificationOutletComponent {
         notification.setAttribute('data-position', position);
         document.body.appendChild(notification);
         this.notifications.push(notification);
+
+        switch (position) {
+            case NotificationPosition.TOP_RIGHT:
+                notification.style.top = `${20 + (this.notifications.length - 1) * 50}px`;
+                notification.style.right = '20px';
+                break;
+            case NotificationPosition.TOP_LEFT:
+                notification.style.top = `${20 + (this.notifications.length - 1) * 60}px`;
+                notification.style.left = '20px';
+                break;
+            case NotificationPosition.BOTTOM_RIGHT:
+                notification.style.bottom = `${20 + (this.notifications.length - 1) * 60}px`;
+                notification.style.right = '20px';
+                break;
+            case NotificationPosition.BOTTOM_LEFT:
+                notification.style.bottom = `${20 + (this.notifications.length - 1) * 60}px`;
+                notification.style.left = '20px';
+                break;
+        }
     }
 }
