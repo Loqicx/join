0480ce2db54001824a17f4b918b3738c7e0c8559
miles <miles.pickull@web.de>
2025-09-09 23:05:39 +0200

    fix(logout): added unsubscribe on Destroy

--- src/app/shared/logout/logout.component.ts
+++ src/app/shared/logout/logout.component.ts
@@ -1,33 +1,45 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
 import { UserService } from '../services/firebase/user.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { LoginService } from '../services/app-login-service.service';
 import { NotificationService } from '../services/notification.service';
 import { NotificationPosition, NotificationType } from '../interfaces/notification';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-logout',
+    standalone: true,
     imports: [CommonModule],
     templateUrl: './logout.component.html',
     styleUrl: './logout.component.scss',
 })
-export class LogoutComponent {
-    userService = inject(UserService);
-    loginService = inject(LoginService);
-    notificationService = inject(NotificationService);
-    router = inject(Router);
+export class LogoutComponent implements OnDestroy {
+    private userService = inject(UserService);
+    private loginService = inject(LoginService);
+    private notificationService = inject(NotificationService);
+    private router = inject(Router);
+
+    private logoutSubscription?: Subscription;
     loggedOut: boolean = false;
 
     ngOnInit() {
         this.router.navigateByUrl('/');
-        this.userService.logout().subscribe({
+        this.logoutSubscription = this.userService.logout().subscribe({
             next: () => {
                 this.loggedOut = true;
                 this.loginService.resetState();
                 this.loginService.verifyLogIn();
-                this.notificationService.pushNotification('Successfully logged out!', NotificationType.SUCCESS, NotificationPosition.BOTTOM_RIGHT);
+                this.notificationService.pushNotification(
+                    'Successfully logged out!',
+                    NotificationType.SUCCESS,
+                    NotificationPosition.BOTTOM_RIGHT
+                );
             },
         });
+    }
+
+    ngOnDestroy() {
+        this.logoutSubscription?.unsubscribe();
     }
 }
