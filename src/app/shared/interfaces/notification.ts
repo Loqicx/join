d214099db3701d51c25c71a8c2cda15da5fba942
CHXST <christopher.haensch@web.de>
2025-09-09 13:30:31 +0200

    feat(notification): add NotificationPosition enum

--- src/app/shared/interfaces/notification.ts
+++ src/app/shared/interfaces/notification.ts
@@ -4,6 +4,13 @@ export enum NotificationType {
     WARNING = 'warning',
 }
 
+export enum NotificationPosition {
+    TOP_RIGHT = 'top-right',
+    TOP_LEFT = 'top-left',
+    BOTTOM_LEFT = 'bottom-left',
+    BOTTOM_RIGHT = 'bottom-right',
+}
+
 export interface Notification {
     message: string;
     type: NotificationType;
