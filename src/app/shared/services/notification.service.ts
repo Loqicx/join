0f42d2abae7068c37234a98cdd947e01c8ea8ff6
CHXST <christopher.haensch@web.de>
2025-09-09 13:33:57 +0200

    feat(notification) add initialNotification for behaviorSubject

--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Notification, NotificationPosition, NotificationType } from '../interfaces/notification';
 
 @Injectable({
     providedIn: 'root',
 })
 export class NotificationService {
+    initialNotification: Notification = {
+        message: '',
+        type: NotificationType.SUCCESS,
+        position: NotificationPosition.TOP_RIGHT,
+        duration: 4000,
+    };
     constructor() {}
 }
